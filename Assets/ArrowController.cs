951d78e3e71c05b6a86bb62302a965b3e80c0d95
kingbj940429 <kingbj0429@gmail.com>
2020-05-28 13:33:18 +0900

    add unity files

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player=GameObject.Find("player");//불러오고
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-0.1f,0);
+        if(transform.position.y<-5.0f){
+            Destroy(gameObject);
+        }
+        Vector2 a=transform.position;
+        Vector2 b=this.player.transform.position;
+        Vector2 d=a-b;//점과 점 사이의 위치벡터
+        float dir=d.magnitude;//거리값 구하기
+        float r1=0.5f;
+        float r2=1.0f;
+
+        if(dir<r1+r2){
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/ArrowGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+   public GameObject arrowPrefab;
+   float span=0.3f;
+   float delta=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      delta+=Time.deltaTime;
+      if(this.delta>this.span){
+          this.delta=0;
+          GameObject go = Instantiate(arrowPrefab) as GameObject;
+          int px=Random.Range(-6,7);
+          go.transform.position=new Vector3(px,7,0);
+      }
+    }
+}
